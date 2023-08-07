446a4a766c2982cda2f53bc81e5670200168d14d
신이삭 <isac9305@gmail.com>
2023-08-07 16:41:57 +0900

    Initial commit.

---
+++ FeedMap/FeedMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedMapApp.swift
+//  FeedMap
+//
+//  Created by 신이삭 on 2023/08/07.
+//
+
+import SwiftUI
+
+@main
+struct FeedMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
